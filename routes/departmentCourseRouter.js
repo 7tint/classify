26d06d6293c995dc7c65592d115cc56aca1c6488
Lambert Liu <lambert0928@outlook.com>
2020-10-16 22:12:30 -0400

    classify courses ui started (not yet functional)

--- routes/departmentCourseRouter.js
+++ routes/departmentCourseRouter.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
+const Department = require("./../models/departmentModel");
+const Course = require('./../models/courseModel');
 
 router.get("/", function(req, res) {
-  res.render("relations/department-course");
+  Department.find({}, (err, departments) => {
+		if (err) {
+			console.log(err);
+		}
+		else {
+      Course.find({}, function(err, courses) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          res.render("relations/department-course", {departments, courses});
+        }
+      });
+		}
+	});
 });
 
 module.exports = router;
