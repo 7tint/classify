5fcd13395f8628b6d4c59e9bfa2b2b3b63b0e68a
Lambert Liu <lambert0928@outlook.com>
2021-04-24 19:09:19 -0400

    new pages for teacher reviews

--- client/src/main.js
+++ client/src/main.js
@@ -18,7 +18,6 @@ import CourseNew from "./views/courses/CourseNew.vue";
 import CourseShow from "./views/courses/CourseShow.vue";
 import CourseEdit from "./views/courses/CourseEdit.vue";
 // import CourseManage from "./views/courses/CourseManage.vue";
-
 import CourseReviewNew from "./views/reviews/CourseReviewNew.vue";
 import CourseReviewEdit from "./views/reviews/CourseReviewEdit.vue";
 
@@ -31,6 +30,8 @@ import TeacherIndex from "./views/teachers/TeacherIndex.vue";
 import TeacherNew from "./views/teachers/TeacherNew.vue";
 import TeacherShow from "./views/teachers/TeacherShow.vue";
 import TeacherEdit from "./views/teachers/TeacherEdit.vue";
+import TeacherReviewNew from "./views/reviews/TeacherReviewNew.vue";
+import TeacherReviewEdit from "./views/reviews/TeacherReviewEdit.vue";
 
 import PreferenceIndex from "./views/preferences/PreferenceIndex.vue";
 import PreferenceEdit from "./views/preferences/PreferenceEdit.vue";
@@ -44,16 +45,20 @@ const routes = [
   {name: "courseReviewNew", path: "/courses/:code/new", component: CourseReviewNew},
   {name: "courseReviewEdit", path: "/courses/:code/:id", component: CourseReviewEdit},
   // {name: "courseManage", path: "/courses/manage", component: CourseManage}
-  {name: "departmentIndex", path: "/departments/", component: DepartmentIndex},
-  {name: "departmentNew", path: "/departments/new", component: DepartmentNew},
-  {name: "departmentShow", path: "/departments/:name", component: DepartmentShow},
-  {name: "departmentEdit", path: "/departments/:name/edit", component: DepartmentEdit},
   {name: "teacherIndex", path: "/teachers/", component: TeacherIndex},
   {name: "teacherNew", path: "/teachers/new", component: TeacherNew},
   {name: "teacherShow", path: "/teachers/:name", component: TeacherShow},
   {name: "teacherEdit", path: "/teachers/:name/edit", component: TeacherEdit},
+  {name: "teacherReviewNew", path: "/teachers/:name/new", component: TeacherReviewNew},
+  {name: "teacherReviewEdit", path: "/teachers/:name/:id", component: TeacherReviewEdit},
+
+  {name: "departmentIndex", path: "/departments/", component: DepartmentIndex},
+  {name: "departmentNew", path: "/departments/new", component: DepartmentNew},
+  {name: "departmentShow", path: "/departments/:name", component: DepartmentShow},
+  {name: "departmentEdit", path: "/departments/:name/edit", component: DepartmentEdit},
+
   {name: "preferenceIndex", path: "/preferences/", component: PreferenceIndex},
-  {name: "preferenceEdit", path: "/preferences/edit", component: PreferenceEdit},
+  {name: "preferenceEdit", path: "/preferences/edit", component: PreferenceEdit}
 ];
 
 const router = new VueRouter({mode: 'history', routes: routes});
