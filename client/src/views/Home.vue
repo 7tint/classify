ddfd2c5db58a178f792e1b4cbf41c257e8bf8639
Lambert Liu <lambert0928@outlook.com>
2021-04-24 22:28:52 -0400

    custom css colors & shadows

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,20 @@
 <template lang="html">
   <div class="">
-    <router-link class="btn btn-primary" :to="{name: 'preferenceIndex'}">Preferences</router-link>
+    <router-link class="btn m-4 btn-primary" :to="{name: 'preferenceIndex'}">Preferences</router-link>
     <br/>
-    <router-link class="btn btn-primary" :to="{name: 'courseIndex'}">Courses</router-link>
+    <router-link class="btn m-4 btn-secondary" :to="{name: 'courseIndex'}">Courses</router-link>
     <br/>
-    <router-link class="btn btn-primary" :to="{name: 'teacherIndex'}">Teachers</router-link>
+    <router-link class="btn m-4 btn-success" :to="{name: 'teacherIndex'}">Teachers</router-link>
     <br/>
-    <router-link class="btn btn-primary" :to="{name: 'departmentIndex'}">Departments</router-link>
+    <router-link class="btn m-4 btn-danger" :to="{name: 'departmentIndex'}">Departments</router-link>
+    <br/>
+    <router-link class="btn m-4 btn-warning" :to="{name: 'home'}">No Shadow</router-link>
+    <br/>
+    <router-link class="btn m-4 btn-info shadow-sm" :to="{name: 'home'}">Small Shadow</router-link>
+    <br/>
+    <router-link class="btn m-4 btn-light shadow" :to="{name: 'home'}">Medium Shadow</router-link>
+    <br/>
+    <router-link class="btn m-4 btn-dark shadow-lg" :to="{name: 'home'}">Large Shadow</router-link>
   </div>
 </template>
 
